a1d098c42e6da6b9dc046425dc71929d14b4e5ce
Jhappy77 <joel.happ@shaw.ca>
2023-01-23 22:03:44 -0700

    Replace browser with chrome

--- options.js
+++ options.js
@@ -2,7 +2,7 @@ const saveButton = document.getElementById("save-button");
 const videoPasscode = document.getElementById("video-passcode");
 
 saveButton.addEventListener("click", () => {
-  browser.storage.sync.set({ videoPasscode: videoPasscode.value }, () => {
+  chrome.storage.sync.set({ videoPasscode: videoPasscode.value }, () => {
     alert("Options saved.");
   });
 });
